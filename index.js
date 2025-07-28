c500c069d9a56385f1715bb48952903d6dbc73d1
Anisur369 <anisurrahman57136@gmail.com>
2025-07-29 01:37:53 +0600

    commit

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-const people = require('./people..js');
-const _ = require('lodash');
-const randomPerson = _.sample(people);
-console.log(randomPerson);
-console.log(_.last(people));
-console.log(__dirname);
-console.log(__filename);
-console.log("======================");
+const http = require("http");
 
-const path = require('path');
-const filePath = path.join(__dirname, 'people..js');
-console.log(filePath);
-console.log(path.basename(filePath));
-console.log(path.dirname(filePath));
-console.log(path.extname(filePath));
-console.log(path.parse(filePath));
-console.log("======================");
+const server = http.createServer((req, res) => {
+  if(req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>hello world bangladesh <a href='/about'>about</a></h1>");
+    res.end();
+  }else if(req.url === "/about") {
+    res.write("<h1>about page <a href='/'>home</a></h1>");
+    res.end();
+  }else {
+    res.write("404 not found");
+    res.end();
+  }
+});
+server.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
-const os = require('os');
-console.log(os.platform());
-console.log(os.homedir());
-console.log(os.freemem());
-console.log(os.cpus());
-console.log("======================");
-
-const fs = require('fs');
-// fs.writeFileSync('./people..js', 'const people = ["sakib", "anis", "Rony"];\n\nmodule.exports = people;');
-// fs.readFile('./people..js', 'utf8', (err, data) => {
-  //   if (err) {
-    //     console.log(err);
-    //   } else {
-      //     let x = data;
-      //     console.log(x);
-//   }
-// });
-let data = fs.readFileSync('./people..js');
-console.log(data.toString());
-console.log("======================");
+server.listen(5000, () => {
+  console.log("http://localhost:5000");
+});
